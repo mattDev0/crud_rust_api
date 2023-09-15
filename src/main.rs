72712799e7a79b25142e9c8ba123e039ddab68fa
mattDev0 <amatthew642@gmail.com>
2023-09-15 12:39:11 +0100

    set necessary contants

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,14 @@ struct Person {
     email: String,
 }
 
+// set database url
+const DB_URL: &str = env!("DATABASE_URL");
+
+// set server address as constant
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+
 fn main(){
 
 }
