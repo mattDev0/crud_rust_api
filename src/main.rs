27843bc3f75a99a82625e1077b9d6eaf4e25e8c8
mattDev0 <amatthew642@gmail.com>
2023-09-15 12:35:43 +0100

    struct our Person model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ Read, Write };
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+// create person model
+#[derive(Serialize, Deserialize)]
+struct Person {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+
+fn main(){
+
 }
